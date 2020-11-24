ce278ae596c16e35822078be1de02a55a91d177a
HubCodes <krlrhkstk12@gmail.com>
2020-11-24 21:08:29 +0900

    Introduce parser state object

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -1,18 +1,22 @@
+mod state;
+
 #[macro_use] extern crate lalrpop_util;
 #[macro_use] extern crate lazy_static;
 
+use state::State;
 use lang::ast::Term;
 use lalrpop_util::{ParseError, lexer};
 
 lalrpop_mod!(grammar);
 
 pub fn parse(code: &str) -> Result<Term, ParseError<usize, lexer::Token, &str>> {
-    let mut symbol_id = 0;
-    grammar::TermParser::new().parse(&mut symbol_id, code)
+    let mut state = State::new();
+    grammar::TermParser::new().parse(&mut state, code)
 }
 
 #[cfg(test)]
 mod tests {
+    use super::state::State;
     use super::grammar;
     use lang::ast::*;
 
@@ -23,79 +27,79 @@ mod tests {
 
     #[test]
     fn positive_int() {
-        let mut symbol_id = 0;
-        let parse_result = TERM_PARSER.parse(&mut symbol_id, "42").unwrap();
+        let mut state = State::new();
+        let parse_result = TERM_PARSER.parse(&mut state, "42").unwrap();
         assert_eq!(Term::Num(Num::Int(42)), parse_result);
     }
 
     #[test]
     #[should_panic]
     fn negative_int() {
-        let mut symbol_id = 0;
-        TERM_PARSER.parse(&mut symbol_id, "-42").unwrap();
+        let mut state = State::new();
+        TERM_PARSER.parse(&mut state, "-42").unwrap();
     }
 
     #[test]
     fn too_big_int_into_double() {
-        let mut symbol_id = 0;
-        let parse_result = TERM_PARSER.parse(&mut symbol_id, "12345678912345678912345").unwrap();
+        let mut state = State::new();
+        let parse_result = TERM_PARSER.parse(&mut state, "12345678912345678912345").unwrap();
         assert_eq!(Term::Num(Num::Double(12345678912345678912345.)), parse_result);
     }
 
     #[test]
     fn positive_double() {
-        let mut symbol_id = 0;
-        let parse_result = TERM_PARSER.parse(&mut symbol_id, "42.24").unwrap();
+        let mut state = State::new();
+        let parse_result = TERM_PARSER.parse(&mut state, "42.24").unwrap();
         assert_eq!(Term::Num(Num::Double(42.24)), parse_result);
     }
 
     #[test]
     fn double_with_exp() {
-        let mut symbol_id = 0;
-        let parse_result = TERM_PARSER.parse(&mut symbol_id, "42.2e-3").unwrap();
+        let mut state = State::new();
+        let parse_result = TERM_PARSER.parse(&mut state, "42.2e-3").unwrap();
         assert_eq!(Term::Num(Num::Double(42.2e-3)), parse_result);
     }
 
     #[test]
     fn string_literal_singlequote() {
-        let mut symbol_id = 0;
-        let parse_result = TERM_PARSER.parse(&mut symbol_id, "'Hello, world!'").unwrap();
+        let mut state = State::new();
+        let parse_result = TERM_PARSER.parse(&mut state, "'Hello, world!'").unwrap();
         assert_eq!(Term::Str("Hello, world!".to_string()), parse_result);
     }
 
     #[test]
     fn string_literal_doublequote() {
-        let mut symbol_id = 0;
-        let parse_result = TERM_PARSER.parse(&mut symbol_id, "\"Hello, world!\"").unwrap();
+        let mut state = State::new();
+        let parse_result = TERM_PARSER.parse(&mut state, "\"Hello, world!\"").unwrap();
         assert_eq!(Term::Str("Hello, world!".to_string()), parse_result);
     }
 
     #[test]
     fn string_literal_empty() {
-        let mut symbol_id = 0;
-        let parse_result = TERM_PARSER.parse(&mut symbol_id, "''").unwrap();
+        let mut state = State::new();
+        let parse_result = TERM_PARSER.parse(&mut state, "''").unwrap();
         assert_eq!(Term::Str("".to_string()), parse_result);
     }
 
     #[test]
     fn string_literal_one_char() {
-        let mut symbol_id = 0;
-        let parse_result = TERM_PARSER.parse(&mut symbol_id, "'1'").unwrap();
+        let mut state = State::new();
+        let parse_result = TERM_PARSER.parse(&mut state, "'1'").unwrap();
         assert_eq!(Term::Str("1".to_string()), parse_result);
     }
 
     #[test]
     fn symbol() {
-        let mut symbol_id = 0;
-        let parse_result = TERM_PARSER.parse(&mut symbol_id, "name").unwrap();
+        let mut state = State::new();
+        let parse_result = TERM_PARSER.parse(&mut state, "name").unwrap();
         assert_eq!(Term::Symbol(Symbol { id: 0, name: "name".to_string() }), parse_result);
     }
 
     #[test]
-    fn two_symbols() {
-        let mut symbol_id = 0;
-        let first_parse_result = TERM_PARSER.parse(&mut symbol_id, "name1").unwrap();
-        let second_parse_result = TERM_PARSER.parse(&mut symbol_id, "name2").unwrap();
+    fn two_symbols_share_same_state() {
+        let mut state = State::new();
+        let first_parse_result = TERM_PARSER.parse(&mut state, "name1").unwrap();
+        let second_parse_result = TERM_PARSER.parse(&mut state, "name2").unwrap();
         let first_expected = Term::Symbol(Symbol { id: 0, name: "name1".to_string() });
         let second_expected = Term::Symbol(Symbol { id: 1, name: "name2".to_string() });
         assert_eq!(first_expected, first_parse_result);
@@ -104,8 +108,8 @@ mod tests {
 
     #[test]
     fn postfix_expr_indexing() {
-        let mut symbol_id = 0;
-        let parse_result = EXPR_PARSER.parse(&mut symbol_id, "a[1]").unwrap();
+        let mut state = State::new();
+        let parse_result = EXPR_PARSER.parse(&mut state, "a[1]").unwrap();
         let target_symbol = Expr::Term(Term::Symbol(Symbol { id: 0, name: "a".to_string() }));
         let target_index = Expr::Term(Term::Num(Num::Int(1)));
         let expected = Expr::Unop(Unop::Index, Box::new(target_symbol), Box::new(target_index));
