5174136770914c1cc0b6552c4729b0a93d930c63
HubCodes <krlrhkstk12@gmail.com>
2020-12-08 22:50:02 +0900

    Bring immutability

--- sema/src/symtable.rs
+++ sema/src/symtable.rs
@@ -1,36 +1,27 @@
 use core::mem;
+use std::rc::Rc;
 
 use lang::ast::Symbol;
 
 #[derive(Default)]
 pub enum Scope {
     Root,
-    Block { parent: Box<Scope>, items: Vec<Symbol> },
-    Function { parent: Box<Scope>, args: Vec<Symbol> },
+    Block { parent: Rc<Scope>, items: Vec<Symbol> },
+    Function { parent: Rc<Scope>, args: Vec<Symbol> },
 }
 
 pub struct SymbolTable {
-    scope: Box<Scope>,
+    scope: Rc<Scope>,
 }
 
 impl SymbolTable {
     fn new() -> SymbolTable {
-        SymbolTable { scope: Box::new(Scope::Root) }
+        SymbolTable { scope: Rc::new(Scope::Root) }
     }
 
-    fn enter_scope(&mut self) {
-        self.scope = Box::new(Scope::Block { parent: Box::new(*self.scope), items: vec![] });
-    }
-
-    fn exit_scope(&mut self) {
-        match *self.scope {
-            Scope::Root => (),
-            Scope::Block { parent, .. } => {
-                mem::replace(&mut self.scope, parent);
-            },
-            Scope::Function { parent, .. } => {
-                mem::replace(&mut self.scope, parent);
-            },
+    fn enter_scope(&mut self) -> SymbolTable {
+        SymbolTable {
+            scope: Rc::new(Scope::Block { parent: Rc::clone(&self.scope), items: vec![] })
         }
     }
 }
