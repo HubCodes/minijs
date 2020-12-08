6c516c7e61260ef965d93dbb8ba8e6eb056d5926
HubCodes <krlrhkstk12@gmail.com>
2020-12-08 23:01:02 +0900

    Add add_def method

--- sema/src/symtable.rs
+++ sema/src/symtable.rs
@@ -1,33 +1,46 @@
-use core::mem;
+use std::cell::RefCell;
 use std::rc::Rc;
 
 use lang::ast::Symbol;
 
-#[derive(Default)]
 pub enum Scope {
     Root,
-    Block { parent: Rc<Scope>, items: Vec<Symbol> },
-    Function { parent: Rc<Scope>, args: Vec<Symbol> },
+    Block { parent: Rc<RefCell<Scope>>, items: Vec<Symbol> },
+    Function { parent: Rc<RefCell<Scope>>, args: Vec<Symbol> },
 }
 
 pub struct SymbolTable {
-    scope: Rc<Scope>,
+    scope: Rc<RefCell<Scope>>,
 }
 
 impl SymbolTable {
     fn new() -> SymbolTable {
-        SymbolTable { scope: Rc::new(Scope::Root) }
+        SymbolTable { scope: Rc::new(RefCell::new(Scope::Root)) }
     }
 
     fn enter_block(&self) -> SymbolTable {
         SymbolTable {
-            scope: Rc::new(Scope::Block { parent: Rc::clone(&self.scope), items: vec![] })
+            scope: Rc::new(
+                RefCell::new(Scope::Block { parent: Rc::clone(&self.scope), items: vec![] })
+            )
         }
     }
 
     fn enter_function(&self) -> SymbolTable {
         SymbolTable {
-            scope: Rc::new(Scope::Function { parent: Rc::clone(&self.scope), args: vec![] })
+            scope: Rc::new(
+                RefCell::new(Scope::Function { parent: Rc::clone(&self.scope), args: vec![] })
+            )
+        }
+    }
+
+    fn add_def(&mut self, symbol: Symbol) {
+        match &mut *(*self.scope).borrow_mut() {
+            Scope::Root => (),
+            Scope::Block { ref mut items, .. } |
+            Scope::Function { args: ref mut items, .. } => {
+                items.push(symbol);
+            },
         }
     }
 }
