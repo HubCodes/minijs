da3d61d014503ea65e627650230f755eb3388738
Taemin Cha <krlrhkstk12@gmail.com>
2021-05-09 13:14:27 +0900

    Change object organization

--- vm/src/object.rs
+++ vm/src/object.rs
@@ -2,11 +2,17 @@ use lang::ast::Symbol;
 use std::collections::HashMap;
 
 enum Data {
-    Ref(*Symbol),
-    Ptr(*Object),
+    Obj { data: HashMap<String, Object> },
+    Primitive(Primitive),
+}
+
+enum Primitive {
+    Int(i32),
+    Double(f64),
+    Boolean(bool),
 }
 
 pub struct Object {
-    prototype: *Object,
-    data: HashMap<String, Data>,
+    prototype: *mut Object,
+    data: Data,
 }
