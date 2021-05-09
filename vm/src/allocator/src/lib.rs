95c5a4511d8637c3552a49906239b9a22f5757e3
Taemin Cha <krlrhkstk12@gmail.com>
2021-05-09 15:10:57 +0900

    Implement basic alloc logic

--- vm/src/allocator/src/lib.rs
+++ vm/src/allocator/src/lib.rs
@@ -1,7 +1,22 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+pub struct Allocator {
+    size: u64,
+    ptr: *mut u8,
+    last: *mut u8,
+}
+
+impl Allocator {
+    pub fn alloc(&mut self, size: u64) -> *mut u8 {
+        unsafe {
+            let result = self.last;
+            let next_last = self.last.add(size as usize);
+            if self.ptr.add(self.size as usize) <= next_last {
+                self.gc();
+            }
+            result
+        }
+    }
+
+    fn gc(&mut self) {
+        todo!();
     }
 }
